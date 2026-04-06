f2c84afc16575a970b985f97ac8a8a44f814f76a
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-04-06 12:29:14 +0530

    Update Operational_Monitoring.py

--- pages/Operational_Monitoring.py
+++ pages/Operational_Monitoring.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pandas as pd
 from src.data_quality import data_quality_preprocessing
+import plotly.express as px
 st.title("📡 Operational Monitoring")
-
 df = data_quality_preprocessing()
-
 st.subheader("Network vs Error Impact")
-
-st.scatter_chart(df[['Network_Latency_ms', 'Error_Rate_%']])
+fig=px.scatter(df,x='Network_Latency_ms',y='Error_Rate_%',color='Efficiency_Status',title='Network vs Error Impact')
+st.plotly_chart(fig)
 st.subheader("Packet Loss vs Efficiency Proxy")
-
 df['Efficiency_Score'] = df['Production_Speed_units_per_hr'] - df['Error_Rate_%']
-st.scatter_chart(df[['Packet_Loss_%', 'Efficiency_Score']])
+fig=px.scatter(df,x='Packet_Loss_%',y='Efficiency_Score',color='Efficiency_Status',title='Packet Loss vs Efficiency Proxy')
+st.plotly_chart(fig)
