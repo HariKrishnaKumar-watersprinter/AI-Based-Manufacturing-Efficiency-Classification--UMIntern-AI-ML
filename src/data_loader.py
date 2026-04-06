4a95e8456c0f42e30e3cd90b92aeab503ea1032f
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-04-06 12:06:09 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def load_data():
-    os.chdir(r'https://raw.github.com/HariKrishnaKumar-watersprinter/AI-Based-Manufacturing-Efficiency-Classification--UMIntern-AI-ML/main/data/Thales_Group_Manufacturing.csv')
+    os.chdir(r'https://raw.github.com/HariKrishnaKumar-watersprinter/AI-Based-Manufacturing-Efficiency-Classification--UMIntern-AI-ML/main')
     path = os.path.join(os.getcwd(), "data", "Thales_Group_Manufacturing.csv")
     
     df = pd.read_csv(path)
