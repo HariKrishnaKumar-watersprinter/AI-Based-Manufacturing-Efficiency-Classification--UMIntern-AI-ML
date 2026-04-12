66ad6f85857c34730cca1bc0811d32afa8d7ec31
Hari Krishna Kumar <harikrishnakumar368@gmail.com>
2026-04-12 11:22:10 +0530

    Update Machine_EfficiencyTrends.py

--- src/Machine_EfficiencyTrends.py
+++ src/Machine_EfficiencyTrends.py
@@ -2,9 +2,7 @@ import pandas as pd
 from src.data_quality import data_quality_preprocessing
 df=data_quality_preprocessing()
 
-    # -------------------------
-    # 2. Per-Machine Efficiency Trends
-    # -------------------------
+# Per-Machine Efficiency Trends
 def machine_trends():
 
         df_temp = df.copy()
@@ -16,9 +14,7 @@ def machine_trends():
 
         return trend_df
 
-    # -------------------------
-    # 3. Machine Efficiency Distribution
-    # -------------------------
+# Machine Efficiency Distribution
 def machine_efficiency_distribution():
 
         dist = df.groupby('Machine_ID')['Efficiency_Status'].value_counts(normalize=True).unstack().fillna(0)*100
